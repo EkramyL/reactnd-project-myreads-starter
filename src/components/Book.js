fccff2b0ca058bb4695a4c5ef490d5b795f6eacf
Ekramy Latief <ekramyl3000@gmail.com>
2022-06-14 16:15:12 +0200

    feat: Add Book component

---
+++ src/components/Book.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Book = (props) => {
+  const thumb = props.book.imageLinks.smallThumbnail;
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url("${thumb}")`,
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select>
+            <option value="move" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.authers}</div>
+      {console.log(props.book.imageLinks.smallThumbnail)}
+      {console.log(thumb)}
+    </div>
+  );
+};
+
+export default Book;
