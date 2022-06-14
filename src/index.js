174e96a43490504a7bde950d551f73f9481abfa6
Ekramy Latief <ekramyl3000@gmail.com>
2022-06-14 13:23:01 +0200

    feat: Covert to function component react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
